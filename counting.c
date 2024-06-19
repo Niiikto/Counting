224197958cb05d6f0227f9863e6164f6b754cdac
Niiikto <dariyaalimbetova@gmail.com>
2024-06-18 22:07:50 -0700

    Adding the space count

---
+++ counting.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+//void counting(char * word, int SIZE);
+void get_word(char lines[], int maxlen) {
+    printf("Please enter your words: ");
+    char *result;
+    int current_len = 0;
+
+    while ((result = fgets(lines + current_len, maxlen - current_len, stdin)) != NULL) {
+        current_len += strlen(lines + current_len);
+        if (current_len >= maxlen - 1) break;
+    }
+
+    if (ferror(stdin)) {
+        perror("failed to read input");
+        exit(1);
+    }
+
+    return;
+}
+
+int main(void) {
+    const unsigned int THE_SIZE = 50000;
+    char theword[THE_SIZE];
+    unsigned long charCount = 0;
+
+    get_word(theword, THE_SIZE);
+
+    for (int i = 0; theword[i] != '\0'; i++) {
+        if (!isspace(theword[i])) {
+            charCount += 1;
+        }
+    }
+
+    printf("%lu\n", charCount);
+
+    unsigned long wordCount = 0;
+
+    for(int i = 0; theword[i] != '\0'; i++){
+        while (isspace(theword[i]) && theword[i] != '\0') {
+            i++;
+        }
+        if (theword[i] != '\0' && !isspace(theword[i])) {
+            wordCount++;
+        }
+        while (theword[i] != '\0' && !isspace(theword[i])) {
+            i++;
+        }
+    }
+    printf("%lu\n", wordCount);
+
+
+    unsigned long lineCount = 0;
+
+    for(int i = 0; theword[i] != '\0'; i++){
+        if(theword[i] != '\n'){
+            lineCount++;
+        }
+        while (theword[i] != '\n') {
+            i++;
+        }
+    }
+    printf("%lu\n", lineCount);
+
+    unsigned long spaceCount = 0;
+
+    for (int i = 0; theword[i] != '\0'; i++) {
+        if (isspace(theword[i])) {
+            spaceCount += 1;
+        }
+    }
+
+    printf("%lu\n", spaceCount);
+
+    return 0;
+}
